2d316dfb638138e5bd2883dc5ec9a874c86ffd46
Vagnerbs <vagnerb1987@gmail.com>
2025-01-15 22:13:13 -0300

    Conversão de tipos

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,21 @@
 public class Main {
     public static void main(String[] args) {
 
-        /*  Metodo ".equals" é a forma correta de comparar Strings, pois o "=="
-            verifica apenas se duas variaveis apontam para o mesmo objeto na memoria,
-            e não se o conteúdo das Strings são iguais.*/
+        double media = (9.8 + 6.3 + 8) / 3;
+        System.out.println("\nMédia entre 3 valores: " + media);
 
-        String senha = "12345";
-        if (senha.equals("12345")) {
-            System.out.println("Acesso autorizado!");
-        } else {
-            System.out.println("Senha incorreta.");
-        }
+        //Casting explicito- conversão de tipos - decimal para inteiro
+        int classificacao = (int) (media/2);
+        System.out.println(classificacao);
+
+        // casting implícito - Quando o tipo de dado de origem é "compatível" com o tipo de dado de destino
+        int x = 10;
+        double y = x;
+        System.out.println(y);
+
+        // casting explícito - Quando o tipo de dado de origem é "incompatível" com o tipo de dado de destino
+        double a = 10.5;
+        int b = (int) a;
+        System.out.println(b);
     }
 }
