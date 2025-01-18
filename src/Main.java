d816eb5b2fcb417045616b27cdcafc37c5783049
Vagnerbs <vagnerb1987@gmail.com>
2025-01-18 17:14:06 -0300

    If-Else-Tomando decisões

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,29 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de Lançamento: " + anoDeLancamento);
+
+        boolean incluirNoPlano = true;
+
+        double notaDoFilme = 8.1;
+        System.out.println("Nota do Filme: " + notaDoFilme);
 
         double media = (9.8 + 6.3 + 8) / 3;
         System.out.println("\nMédia entre 3 valores: " + media);
 
-        //Casting explicito- conversão de tipos - decimal para inteiro
+        //Text Blocks - Metodo de formatação de String
+        String sinopse;
+        sinopse = """
+                \nFilme top Gun
+                Filme Top Gan de Aventura dos anos 80
+                Muito bom!
+                Ano de Lançamento:""" + anoDeLancamento ;
+        System.out.println(sinopse);
+
+        //Casting - conversão de tipos - decimal para inteiro
         int classificacao = (int) (media/2);
         System.out.println(classificacao);
 
